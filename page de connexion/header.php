e92859843761e8a9d24c896e1a7c5753eaef5484
JeannePreira <jeannepreira95@gmail.com>
2020-04-10 21:23:25 +0100

    ProjetSA

---
+++ page de connexion/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+  
+    <style>
+    body{
+    width:90%;
+    background-image: url('icones/img-bg.jpg');
+    background-repeat:no-repeat;
+     
+}
+.header{
+    width: 108%;
+    height: 50px;
+    background-color: rgb(29, 29, 37); 
+
+}
+img{
+    height: 50px;
+    padding: 3px;
+    float: left;
+}
+ h3{
+    width: 60%;
+    height: 80px;
+    color: white;
+    float: right;
+    margin-top: 10px;
+}
+    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="header">
+            <div class="logo"><img src="icones/logo.png" alt="logo"></div>
+            <div class="titre"><h3>Le plaisir de jouer</h3></div>
+        </div>
+</body>
+</html>
